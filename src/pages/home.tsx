bfe9f529ed6f12eb8860c6b2abca274a6526e290
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-14 03:59:10 +0100

    Created home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import "../App.css";
+import Header from "../components/header/header.component";
+
+function Home() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
