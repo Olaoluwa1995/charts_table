88cc960dcc962b0ee0d91d43572ffdd826fd0513
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-14 05:29:49 +0100

    Added banner component to home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
 import "../App.css";
+import Banner from "../components/banner/banner";
 import Header from "../components/header/header.component";
 
 function Home() {
   return (
     <div className="App">
       <Header />
+      <Banner />
     </div>
   );
 }
