872a69221de83d5c30f16ac78c693bf8caa05354
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-14 01:55:24 +0100

    Chakra ui set up

---
+++ src/styles/theme.ts
@@ -0,0 +1,60 @@
+import { extendTheme } from "@chakra-ui/react";
+import { Styles } from "@chakra-ui/theme-tools";
+
+export const COLORS = {
+  PRIMARY_COLOR: "#004151",
+  WHITE: "#ffffff",
+  TABLE_BUTTON_COLOR: "#808080",
+  BACKGROUND_COLOR: "whitesmoke",
+};
+
+export const BOXSHADOW = "0 4px 6px 0 lightGrey";
+export const BORDER_RADIUS = "10px";
+export const TABLESTYLE = {
+  size: "sm",
+  fontSize: "0.9rem",
+  w: "unset",
+};
+
+const STYLES: Styles = {
+  global: {
+    "*": {
+      boxSizing: "border-box",
+    },
+
+    html: {
+      minH: "100%",
+      overflowX: "hidden",
+    },
+
+    body: {
+      display: "flex",
+      flexDir: "column",
+      overflowX: "hidden",
+      color: "black",
+
+      "#root": {
+        "* .roboto": {
+          fontFamily: "'Poppins', sans-serif",
+        },
+      },
+    },
+
+    "#root": {
+      minH: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      width: "100%",
+      pos: "relative",
+
+      "*": {
+        fontFamily: "'Poppins'",
+      },
+    },
+  },
+};
+
+export const customTheme = extendTheme({
+  styles: STYLES,
+  colors: COLORS,
+});
