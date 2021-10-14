8b1881ca21ac7ce654733abc428ff18f13b94a90
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-14 05:26:21 +0100

    Integrated responsiveness

--- src/components/custom-button/custom-button.component.tsx
+++ src/components/custom-button/custom-button.component.tsx
@@ -13,14 +13,14 @@ const CustomButton: React.FC<CustomButtonProps> = ({
   return (
     <Button
       overflowX="hidden"
-      rounded="15px"
-      fontSize={{ base: "0.7rem", md: "0.9rem" }}
+      rounded={{ base: "10px", md: "15px" }}
+      fontSize={{ base: "0.7rem", md: "0.8rem" }}
       border="1px solid white"
       boxShadow="0px 8px 5px rgba(75, 93, 104, 0.1)"
       bgColor={COLORS.PRIMARY_COLOR}
       color="white"
       fontWeight="medium"
-      h="3rem"
+      h={{ base: "2rem", md: "2.5rem" }}
       _focus={{
         border: "none",
       }}
