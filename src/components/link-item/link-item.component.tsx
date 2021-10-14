14666e31c0321a430c4cf35bb2af3f86595b4667
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-14 03:58:37 +0100

    Clean up

--- src/components/link-item/link-item.component.tsx
+++ src/components/link-item/link-item.component.tsx
@@ -4,39 +4,41 @@ import { Link, NavLink } from "react-router-dom";
 import { COLORS } from "../../styles/theme";
 
 export interface LinkItemProps {
-	isLink?: boolean;
-	isAnchor: boolean;
-	url: any;
-	children: any;
-	hoverStyles?: BoxProps;
-	hoverProps?: BoxProps;
+  isLink?: boolean;
+  isAnchor: boolean;
+  url: any;
+  children: any;
+  hoverStyles?: BoxProps;
+  hoverProps?: BoxProps;
 }
 
 export type LinkProps = LinkItemProps & BoxProps & LinkBoxProps;
 
 const LinkItem: React.FC<LinkProps> = ({
-	isLink = true,
-	isAnchor = true,
-	children,
-	url,
-	hoverProps,
-	...otherProps
+  isLink = true,
+  isAnchor = true,
+  children,
+  url,
+  hoverProps,
+  ...otherProps
 }) => {
-	return (
-		<Box
-			as={isAnchor ? "a" : isLink ? Link : NavLink}
-			{...(isAnchor ? { href: url } : { to: url })}
-			activeStyle={{ color: `${COLORS.PRIMARY_COLOR}` }}
-			exact={true}
-			to={url}
-			fontSize={{ base: "0.8rem", lg: "0.9rem" }}
-			textDecoration="none"
-			_hover={{ textDecoration: "none", color: "inherit", ...hoverProps }}
-			color="blackAlpha.800"
-			{...otherProps}>
-			{children}
-		</Box>
-	);
+  return (
+    <Box
+      as={isAnchor ? "a" : isLink ? Link : NavLink}
+      {...(isAnchor ? { href: url } : { to: url })}
+      activeStyle={{ color: `${COLORS.PRIMARY_COLOR}` }}
+      exact={true}
+      to={url}
+      fontSize={{ base: "0.8rem", md: "1rem", lg: "1.2rem" }}
+      fontWeight="medium"
+      textDecoration="none"
+      _hover={{ textDecoration: "none", color: "inherit", ...hoverProps }}
+      color="blackAlpha.800"
+      {...otherProps}
+    >
+      {children}
+    </Box>
+  );
 };
 
 export default React.memo(LinkItem);
