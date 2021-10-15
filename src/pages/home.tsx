6f8741e35d18722868ccf260dc06e3aad6b3ba2e
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-15 17:30:22 +0100

    Added featured-on component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import Banner from "../components/banner/banner";
 import Benefits from "../components/benefits/benefits";
 import CustomerService from "../components/customer-service/customer-service";
 import Expectations from "../components/expectations/expectations";
+import AsFeaturedOn from "../components/featured-on/featured-on";
 import Features from "../components/features/features";
 import Header from "../components/header/header.component";
 import Story from "../components/story/story";
@@ -17,6 +18,7 @@ function Home() {
       <Story />
       <CustomerService />
       <Expectations />
+      <AsFeaturedOn />
     </div>
   );
 }
