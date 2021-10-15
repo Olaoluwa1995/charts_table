892af28be9d73a036aedf0e0cf32d01aa9f07205
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-15 17:28:37 +0100

    Created featured-on component

---
+++ src/components/featured-on/featured-on.tsx
@@ -0,0 +1,56 @@
+import { Box, Flex, Icon, Image } from "@chakra-ui/react";
+import React from "react";
+import { BOXSHADOW, COLORS } from "../../styles/theme";
+import CustomerServiceImage from "../../assets/customer-service.png";
+import Aos from "aos";
+import { HiChevronDoubleRight } from "react-icons/hi";
+import { featuredOns, FeaturedOn } from "./featured-on-data";
+
+function AsFeaturedOn() {
+  React.useEffect(() => {
+    Aos.init({ duration: 2000, once: true });
+  }, []);
+  return (
+    <Flex
+      flexDir="column"
+      bgColor={COLORS.BACKGROUND_COLOR}
+      minH={{ base: "10rem", sm: "12rem" }}
+      width="100%"
+      py="5%"
+      px={{ base: "5%", sm: "10%" }}
+      justify="space-between"
+    >
+      <Box
+        w="100%"
+        textTransform="uppercase"
+        color={COLORS.PRIMARY_COLOR}
+        fontSize="xl"
+        fontWeight="semibold"
+        textAlign={{ base: "center", sm: "start" }}
+        mb="5%"
+      >
+        AS FEATURED ON
+      </Box>
+      <Flex flexWrap="wrap" mt={{ base: "0.7rem", md: "1rem" }}>
+        {featuredOns.map((featuredOn: FeaturedOn) => {
+          return (
+            <Box
+              key={featuredOn?.key}
+              w={{ base: "42%", sm: "29.3%", md: "20%" }}
+              mx={{ base: "4%", sm: "2%", md: "2.5%" }}
+              h={{ base: "3rem", sm: "3.5rem", md: "4rem" }}
+              mb={{ base: "1rem", md: "2rem" }}
+              _hover={{
+                boxShadow: BOXSHADOW,
+              }}
+            >
+              <Image src={featuredOn.image} alt="" h="100%" w="100%" />
+            </Box>
+          );
+        })}
+      </Flex>
+    </Flex>
+  );
+}
+
+export default AsFeaturedOn;
