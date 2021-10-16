2a96ae952dae320cbb6291270cb1c0a6298f730e
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-16 22:31:24 +0100

    Created follow us data

---
+++ src/components/blog/follow-us/follow-us-data.tsx
@@ -0,0 +1,24 @@
+import { Social } from "../../footer/footer-data";
+import Instagram from "../../../assets/colored-instagram.png";
+import LinkedIn from "../../../assets/colored-linkedin.png";
+import Facebook from "../../../assets/colored-facebook.png";
+import Twitter from "../../../assets/colored-twitter.png";
+
+export const coloredSocials: Social[] = [
+  {
+    key: 1,
+    image: Facebook,
+  },
+  {
+    key: 2,
+    image: Instagram,
+  },
+  {
+    key: 3,
+    image: LinkedIn,
+  },
+  {
+    key: 4,
+    image: Twitter,
+  },
+];
