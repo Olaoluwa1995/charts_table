39d243d3d36e047dc61a758ebaa6fb499dc7af4a
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-16 12:05:42 +0100

    Created footer column

---
+++ src/components/footer/footer-column.tsx
@@ -0,0 +1,30 @@
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+import { Item } from "./footer-data";
+
+type Props = {
+  title: string;
+  list: Item[];
+};
+
+const FooterColumn: React.FC<Props> = ({ list, title }) => {
+  return (
+    <Flex flexDir="column" align="flex-start">
+      <Box mb="1.5rem" fontSize="xs" fontWeight="medium">
+        {title}
+      </Box>
+      {list.map((feature: Item) => (
+        <Box
+          mb="1.3rem"
+          textAlign="start"
+          fontSize="xx-small"
+          key={feature.key}
+        >
+          {feature.name}
+        </Box>
+      ))}
+    </Flex>
+  );
+};
+
+export default FooterColumn;
