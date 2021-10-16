71bde74c963dac4d4b5448610a000b3df8e20f86
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-16 12:13:15 +0100

    Added animation data

--- src/components/featured-on/featured-on-data.ts
+++ src/components/featured-on/featured-on-data.ts
@@ -10,39 +10,48 @@ import TechpointImage from "../../assets/techpoint.png";
 export type FeaturedOn = {
   key: number;
   image: string;
+  delay: string;
 };
 
 export const featuredOns: FeaturedOn[] = [
   {
     key: 1,
     image: BusinessImage,
+    delay: "0",
   },
   {
     key: 2,
     image: PunchImage,
+    delay: "200",
   },
   {
     key: 3,
     image: LindaImage,
+    delay: "400",
   },
   {
     key: 4,
     image: BBCImage,
+    delay: "600",
   },
   {
     key: 5,
     image: DisruptImage,
+    delay: "800",
   },
   {
     key: 6,
     image: GuardianImage,
+    delay: "1000",
   },
   {
     key: 7,
     image: BellaImage,
+    delay: "1200",
   },
   {
     key: 8,
     image: TechpointImage,
+    delay: "1400",
   },
 ];
