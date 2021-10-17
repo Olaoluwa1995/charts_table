9260e90ea9baad94f9fc4b8725184ec4f84e7403
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-17 17:25:14 +0100

    Created faq data

---
+++ src/components/faqs/faq-data.tsx
@@ -0,0 +1,80 @@
+export type FAQ = {
+  key: number;
+  question: string;
+  answer: string;
+};
+
+export const faqs: FAQ[] = [
+  {
+    key: 1,
+    question: "What is Paymyrent?",
+    answer:
+      "Paymyrent helps makes house rent saving seamless. Paymyrent takes away the stress and planning required to save on a regular basis through its automated and easy-to-use platform. Your savings also generates interests, up to 20% per annum.",
+  },
+  {
+    key: 2,
+    question: "Why should I save on Paymyrent?",
+    answer: "",
+  },
+  {
+    key: 3,
+    question: "How do I start saving on Paymyrent?",
+    answer: "",
+  },
+  {
+    key: 4,
+    question: "How much can I start saving with?",
+    answer: "",
+  },
+  {
+    key: 5,
+    question: "Does Paymyrent have an office?",
+    answer: "",
+  },
+  {
+    key: 6,
+    question: "Are my card details safe?",
+    answer: "",
+  },
+  {
+    key: 7,
+    question: "How secure is my money?",
+    answer: "",
+  },
+  {
+    key: 8,
+    question: "Do I need to login each time I want to save?",
+    answer: "",
+  },
+  {
+    key: 9,
+    question:
+      "What happens if I missed some savings and I now have funds for that?",
+    answer: "",
+  },
+  {
+    key: 10,
+    question: "Can I pause my savings and continue anytime?",
+    answer: "",
+  },
+  {
+    key: 11,
+    question: "What if I don’t have funds in my account to debit?",
+    answer: "",
+  },
+  {
+    key: 12,
+    question: "What is quicksave and how does it work?",
+    answer: "",
+  },
+  {
+    key: 13,
+    question: "Do I get interest on Paymyrent?",
+    answer: "",
+  },
+  {
+    key: 14,
+    question: "What does 7% per deposit mean?",
+    answer: "",
+  },
+];
