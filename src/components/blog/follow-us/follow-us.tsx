54fa4a71146125ae8de7e8c8c55c39b31d34b501
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-17 07:03:17 +0100

    Updated design

--- src/components/blog/follow-us/follow-us.tsx
+++ src/components/blog/follow-us/follow-us.tsx
@@ -12,19 +12,23 @@ function FollowUs() {
       bgColor="white"
       flexDir="column"
     >
-      <Box mb="0.8rem" fontSize="xs" fontWeight="medium">
+      <Box
+        mb="0.8rem"
+        fontSize={{ base: "xs", md: "sm", xl: "md" }}
+        fontWeight="medium"
+      >
         Follow us on social media
       </Box>
-      <Box mb="3rem" fontSize="xx-small">
+      <Box mb="3rem" fontSize={{ base: "xx-small", md: "xs", xl: "sm" }}>
         Stay updated with the latest on all things Paymyrent
       </Box>
       <Flex>
         {coloredSocials.map((social: Social) => (
           <Image
-            mr="2rem"
+            mr={{ base: "1rem", md: "2rem" }}
             key={social.key}
-            h={{ base: "1.2rem", md: "1.5rem" }}
-            w={{ base: "1.2rem", md: "1.5rem" }}
+            h={{ base: "1.2rem", md: "1.5rem", xl: "2rem" }}
+            w={{ base: "1.2rem", md: "1.5rem", xl: "2rem" }}
             src={social.image}
           />
         ))}
