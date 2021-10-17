2559d7c03a5a362239dca571fd6292bf99d4b1e1
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-17 17:25:32 +0100

    Created faqs

---
+++ src/pages/faqs.tsx
@@ -0,0 +1,15 @@
+import FAQContent from "../components/faqs/faq-content";
+import Footer from "../components/footer/footer";
+import Header from "../components/header/header.component";
+
+function FAQs() {
+  return (
+    <>
+      <Header />
+      <FAQContent />
+      <Footer />
+    </>
+  );
+}
+
+export default FAQs;
