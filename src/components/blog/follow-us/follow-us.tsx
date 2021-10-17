3a35e09b4eec3fed425c04bf1a1f1729acac34c7
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-17 17:24:31 +0100

    Added animation

--- src/components/blog/follow-us/follow-us.tsx
+++ src/components/blog/follow-us/follow-us.tsx
@@ -1,8 +1,13 @@
 import { Box, Flex, Image } from "@chakra-ui/react";
+import Aos from "aos";
+import React from "react";
 import { Social } from "../../footer/footer-data";
 import { coloredSocials } from "./follow-us-data";
 
 function FollowUs() {
+  React.useEffect(() => {
+    Aos.init({ duration: 2000, once: true });
+  }, []);
   return (
     <Flex
       w="100%"
@@ -14,21 +19,31 @@ function FollowUs() {
     >
       <Box
         mb="0.8rem"
-        fontSize={{ base: "xs", md: "sm", xl: "md" }}
+        fontSize={{ base: "xs", sm: "sm", md: "md", xl: "lg" }}
         fontWeight="medium"
       >
         Follow us on social media
       </Box>
-      <Box mb="3rem" fontSize={{ base: "xx-small", md: "xs", xl: "sm" }}>
+      <Box
+        mb="3rem"
+        fontSize={{
+          base: "xx-small",
+          sm: "xs",
+          md: "sm",
+          lg: "md",
+        }}
+      >
         Stay updated with the latest on all things Paymyrent
       </Box>
       <Flex>
         {coloredSocials.map((social: Social) => (
           <Image
+            data-aos="fade"
+            data-aos-delay={(social.key * 200).toString()}
             mr={{ base: "1rem", md: "2rem" }}
             key={social.key}
-            h={{ base: "1.2rem", md: "1.5rem", xl: "2rem" }}
-            w={{ base: "1.2rem", md: "1.5rem", xl: "2rem" }}
+            h={{ base: "1.2rem", md: "1.5rem", lg: "2rem" }}
+            w={{ base: "1.2rem", md: "1.5rem", lg: "2rem" }}
             src={social.image}
           />
         ))}
