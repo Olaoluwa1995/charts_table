2a5b4e202ab91df4fe5e2e5841fd38218ceda8b8
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-20 06:23:00 +0100

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import { ChakraProvider, CSSReset, theme } from "@chakra-ui/react";
-import { Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import { customTheme } from "./styles/theme";
 import "aos/dist/aos.css";
-import ContactUs from "./pages/contact-us";
-import FAQs from "./pages/faqs";
 
 function App() {
   return (
     <ChakraProvider theme={{ ...theme, ...customTheme }}>
       <CSSReset />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/faq" component={FAQs} />
-        <Route exact path="/contact-us" component={ContactUs} />
-      </Switch>
+      <Home />
     </ChakraProvider>
   );
 }
