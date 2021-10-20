d215bd93274f4d3be02ed8f3c5fb24502a5e562b
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-20 06:20:34 +0100

    Created bar chart component

---
+++ src/components/bar-chart/bar-chart.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+
+type Props = {
+  keys: any[];
+  sumOfSales: any[];
+};
+
+const BarChart: React.FC<Props> = ({ keys, sumOfSales }) => {
+  return (
+    <Bar
+      data={{
+        labels: keys,
+        datasets: [
+          {
+            label: "Amount of Sales",
+            data: sumOfSales,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      }}
+    />
+  );
+};
+
+export default BarChart;
