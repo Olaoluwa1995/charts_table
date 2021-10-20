f562d419fb61837b7b6c4d54f5773660715e6753
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-10-20 18:47:11 +0100

    Updated design

--- src/components/pie-chart/pie-chart.tsx
+++ src/components/pie-chart/pie-chart.tsx
@@ -24,13 +24,8 @@ const PieChart: React.FC<Props> = ({ keys, sumOfSales }) => {
               "rgba(255, 206, 86, 0.2)",
               "rgba(75, 192, 192, 0.2)",
             ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-            ],
-            borderWidth: 1,
+            borderColor: "white",
+            borderWidth: 2,
           },
         ],
       }}
